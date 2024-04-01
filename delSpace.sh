272e1d3c5746752abdbe5c7a570df31ae3a4cd06
mi <zhangyiming748@gmail.com>
2024-04-01 11:07:34 +0800

    sed命令可以作用于视频文件 使用时需谨慎

--- delSpace.sh
+++ delSpace.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-# 删除每行第一个无效空格
-# 遍历当前目录下的所有文件
-for file in *; do
-  # 检查是否为文件
-  if [ -f "$file" ]; then
+
+# 指定要遍历的文件夹路径
+# sed命令可以作用于视频文件 使用时需谨慎
+folder_path="/f/Telegram/en"
+
+# 遍历文件夹下所有扩展名为txt的文件
+for file in "$folder_path"/*.txt; do
     # 使用sed命令删除每行第一个多余的空格
     sed 's/^[[:space:]]\+//' "$file" > "${file}_temp"
     # 用处理后的文件替换原文件
     mv "${file}_temp" "$file"
-  fi
 done
